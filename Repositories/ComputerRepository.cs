c26cbe847dcc71df5b0d62c49e5a948a65aff27e
weronicamelo <weronicaamelo@gmail.com>
2022-05-27 19:11:38 -0300

    add computer update functionality

--- Repositories/ComputerRepository.cs
+++ Repositories/ComputerRepository.cs
@@ -67,4 +67,26 @@ class ComputerRepository
         command.ExecuteNonQuery();
         connection.Close();
     }
+
+    public Computer Update(Computer computer)
+    {
+        var connection = new SqliteConnection(_databaseConfig.ConnectionString);
+        connection.Open();
+
+        var command = connection.CreateCommand();
+        command.CommandText = @"
+            UPDATE Computers 
+            SET 
+                ram = $ram,
+                processor = $processor
+            WHERE id = $id
+        ";
+        command.Parameters.AddWithValue("$id", computer.Id);
+        command.Parameters.AddWithValue("$ram", computer.Ram);
+        command.Parameters.AddWithValue("$processor", computer.Processor);
+
+        command.ExecuteNonQuery();
+        connection.Close();
+        return computer;
+    }
 }
