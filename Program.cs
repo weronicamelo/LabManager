3b42090460a191b430c7f57148bd8e3eef6ca762
weronicamelo <weronicaamelo@gmail.com>
2022-06-09 16:03:02 -0300

    change in methods and addition of validation

--- Program.cs
+++ Program.cs
@@ -40,17 +40,32 @@ if(modelName == "Computer")
     {
         Console.WriteLine("Computer Delete");
         var id = Convert.ToInt32(args[2]);
-        computerRepository.Delete(id);
+
+        if(computerRepository.ExistsById(id))
+        {
+            computerRepository.Delete(id);
+        }
+        else {
+            Console.WriteLine($"Computador com id {id} não existe");
+        }
     }
         
     if(modelAction == "Update")
     {
         Console.WriteLine("Computer Update");
         var id = Convert.ToInt32(args[2]);
-        var ram = args[3];
-        var processor = args[4];
-        var computer = new Computer(id, ram, processor);
-        computerRepository.Update(computer);
+
+        if(computerRepository.ExistsById(id))
+        {
+            var ram = args[3];
+            var processor = args[4];
+            var computer = new Computer(id, ram, processor);
+
+            computerRepository.Update(computer);
+        }
+        else {
+            Console.WriteLine($"Computador com id {id} não existe");
+        }
     }
 
     if(modelAction == "Show")
