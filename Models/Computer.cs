17714e8152c3c9c7f77535dbb4b7f5c5a1216a1f
weronicamelo <weronicaamelo@gmail.com>
2022-06-28 22:39:22 -0300

    refactor: method update using dapper

--- Models/Computer.cs
+++ Models/Computer.cs
@@ -6,17 +6,14 @@ class Computer
     public string Ram {get; set; }  
     public string Processor {get; set; }
 
+     public Computer() {}
+
     public Computer(int id, string ram, string processor)
     {
         Id = id;
         Ram = ram;
         Processor = processor;
     }
-
-    public Computer()
-    {
-        Id = 0;
-        Ram = "";
-        Processor = "";
-    }
 }
+
+   
